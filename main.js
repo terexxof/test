2ba6502a20f39137708131dad59c73e5e3da3715
terexxof <79165304+terexxof@users.noreply.github.com>
2021-03-02 20:44:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 
-let currentUserName = prompt('Введите ваше имя:', 'Пидор');
+let currentUserName = prompt('Введите ваше имя:', 'Имя');
 
-let pidor = confirm('Ты пидор?');
+let pidor = confirm('У меня крутой скрипт?');
 let pidorResult
 
-if (pidor == 1) (pidorResult = 'пидор')
-    else (pidorResult = 'красавчик');
+if (pidor == 1) (pidorResult = 'молодец')
+    else (pidorResult = 'не молодец');
 
 alert(`Вас зовут ${currentUserName} и вы ${pidorResult}`);
